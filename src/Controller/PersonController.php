9f2fafbbc2e616e3246228b2b7f6d0a7631492ef
MIHAN Zlanca-N'to <mihanzlancanto@gmail.com>
2024-12-25 19:09:50 +0100

    Doctrine : Requête SELECT

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PersonRepository;
 use App\service\PersonService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,14 +12,24 @@ use Symfony\Component\Routing\Attribute\Route;
 class PersonController extends AbstractController
 {
     public function __construct(
-        private readonly PersonService  $personService
+        private readonly PersonService  $personService,
+        private readonly PersonRepository $personRepository
     ){}
+
+    #[Route('/all', name: 'app_person_all')]
+    public function index(): Response
+    {
+        $persons = $this->personRepository->findAll();
+        return $this->render('person/index.html.twig', [
+            'persons' => $persons
+        ]);
+    }
 
     #[Route('/add', name: 'app_person_add')]
     public function add(): Response
     {
         $person = $this->personService->create('Zlanca-Nto', 'MIHAN', 44);
-        return $this->render('person/index.html.twig', [
+        return $this->render('person/person.html.twig', [
             'person' => $person,
         ]);
     }
