8af9393076f9211df1711f691b14acd871e7d4e0
MIHAN Zlanca-N'to <mihanzlancanto@gmail.com>
2024-12-25 18:10:03 +0100

    Doctrine : Fixture et Faker pour générer aléatoirment les données

---
+++ src/DataFixtures/PersonFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\service\PersonService;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class PersonFixture extends Fixture
+{
+    public function __construct(
+        private readonly PersonService $personService,
+    ){}
+
+    /*
+        Commande 'symfony console doctrine:fixture:load --append'
+        pour générer toutes ces données dans la bd
+    */
+    public function load(ObjectManager $manager): void
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+        for ($i = 0; $i < 100; $i++)
+        {
+            $this->personService->create($faker->firstNameMale,
+                $faker->name,
+                $faker->numberBetween($min = 1, $max = 100)
+            );
+        }
+        $manager->flush();
+    }
+}
